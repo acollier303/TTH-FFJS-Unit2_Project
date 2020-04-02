e65ab66352f2d9bfc293ff53d82a4fff7fdbab8f
Adrian Collier <30817514+acollier303@users.noreply.github.com>
2020-04-01 22:15:58 -0600

    updated appendPageLinks()

--- js/script.js
+++ js/script.js
@@ -4,21 +4,21 @@ FSJS project 2 - List Filter and Pagination
 Adrian Collier
 ******************************************/
 
-
+//Global Variables
 const listItems = document.querySelectorAll('li'); // targets all li's on the page
 const itemsPerPage = 10;
+
 
 
 /* Display Page Function - */
 const showPage = (list, page) => {
    // Variables for start and end of list items  from page
    let startIndex = (page * itemsPerPage) - itemsPerPage;
-   let endIndex = (page * itemsPerPage);
+   let endIndex = (page * itemsPerPage)-1;
 
    //Cycles throught list and shows 10 students
    for (let i=0; i<list.length; i+=1){
       if (i >= startIndex && i <= endIndex) {
-         console.log('show');
         list[i].style.display = '';
       }else{
          list[i].style.display = 'none';
@@ -27,13 +27,13 @@ const showPage = (list, page) => {
 }
   
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
+   `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
-
 const appendPageLinks = (list) => {
    const numOfPages = Math.ceil(listItems.length / 10); //rounds number of pages to the next largest integer
-   //console.log(numOfPages);
+   
+   //html elements created
    const mainDiv = document.querySelector('.page');
    const buttonDiv = document.createElement('div');
    const ul = document.createElement('ul');
@@ -41,22 +41,38 @@ const appendPageLinks = (list) => {
    buttonDiv.className = 'pagination';
    mainDiv.appendChild(buttonDiv);
    buttonDiv.appendChild(ul);
-   
+
    //Loop through pages and add li for every page
-   for (i=0; i < numOfPages; i+= 1){
+   for (let i=0; i < numOfPages; i+= 1){
       const li = document.createElement('li');
-      ul.appendChild(li);
-      const button = document.createElement('button');
-      li.appendChild(button);
-      button.innerHTML = `<a href="#">${i+1}</a>`;
+      const link = document.createElement('a');
       
+      ul.appendChild(li);
+      li.appendChild(link);
+
+      //adds page number to each link
+      link.textContent = `${i+1}`;
+      link.setAttribute('class', ''); 
+            
+       if (i === 0){
+           link.setAttribute('class', 'active');
+       };
+
+      //page link event handler
+   };      
+      buttonDiv.addEventListener('click', (e) =>{
+         const currentPage = Number.parseInt(e.target.textContent);
+         console.log(currentPage);
+         console.log(e.target);
+         e.target.setAttribute('class', '');
+         if (e.target){
+            e.target.setAttribute('class', 'active');
+         } else {
+            e.target.sib
+         }     
+                     
+         showPage(listItems, currentPage);
+      });
 
    }
-
-
-   
-}
-
-showPage(listItems, 3); 
 appendPageLinks(listItems);
-   
