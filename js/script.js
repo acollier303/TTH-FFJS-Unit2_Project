486944624fe6eaafac7cdf000734d0b34dfaf781
Adrian Collier <30817514+acollier303@users.noreply.github.com>
2020-04-07 09:33:29 -0600

    Restructered appendPage()

--- js/script.js
+++ js/script.js
@@ -7,23 +7,10 @@ Adrian Collier
 //Global Variables
 const listItems = document.querySelectorAll('li'); // targets all li's on the page
 const itemsPerPage = 10;
-const page = 1;
-
-const activePage = () => {
-   const pageLinks = document.getElementsByTagName('a');
-   for (let i = 0; i< listItems.length; i+=1){
-      if (pageLinks == page){
-         pageLinks[i].setAttribute('class', 'active');
-      } else {
-         pageLinks[i].setAttribute('class', '');
-      }
-      
-   }
-}
+let currentPage = 1;
 
 
-
-/* Display Page Function - */
+/* Show Page Function - */
 const showPage = (list, page) => {
    // Variables for start and end of list items  from page
    let startIndex = (page * itemsPerPage) - itemsPerPage;
@@ -46,7 +33,7 @@ const showPage = (list, page) => {
 ***/
 const appendPageLinks = (list) => {
    const numOfPages = Math.ceil(listItems.length / 10); //rounds number of pages to the next largest integer
-   
+  
    //html elements created
    const mainDiv = document.querySelector('.page');
    const buttonDiv = document.createElement('div');
@@ -58,38 +45,50 @@ const appendPageLinks = (list) => {
 
    //run showPage function to show page 1
    showPage(listItems, 1);
-
-   //Loop through pages and add li for every page
+   
+   //Loop through pages and add li and link for every page
    for (let i=0; i < numOfPages; i+= 1){
       const li = document.createElement('li');
       const link = document.createElement('a');
+      let currentPage = i+1;
       
+      // adds li and link to the DOM
       ul.appendChild(li);
       li.appendChild(link);
 
       //adds page number to each link
-      link.textContent = `${i+1}`;
-      link.setAttribute('class', '');
+      link.textContent = `${currentPage}`;
+      //link.setAttribute('class', '');
       
       // Sets the first li link to active
       ul.firstChild.firstChild.setAttribute('class', 'active');
-        
+      
+         //   //activePage function sets class attribute for links
+         //   const activePage = () => {
+         //    const pageLinks = document.getElementsByTagName('a');
+         //    for (let i = 0; i< pageLinks.length; i+=1){
+         //       if (pageLinks[i] == currentPage){
+         //          pageLinks[i].setAttribute('class', 'active');
+         //       } else {
+         //          pageLinks[i].setAttribute('class', '');
+         //       }
+         //    }
+         // };
    
    };      
       //page link event handler
       buttonDiv.addEventListener('click', (e) =>{
-         const currentPage = Number.parseInt(e.target.textContent);
+         let currentPage = Number.parseInt(e.target.textContent);
          const a = document.getElementsByTagName('a');
          console.log(currentPage);
          console.log(e.target);
-
-     
-         // if (e.target){
-         //    e.target.setAttribute('class', 'active');
-         //    } else {
-         //    ul.children.firstChild.setAttribute('class', '');
-         // }    
-                 
+       
+             if (e.target.className == currentPage){
+            e.target.setAttribute('class', 'active');
+            } else {
+            //e.target.classList.remove('.pagination li a.active');   
+         }
+        
          showPage(listItems, currentPage);
       });
 
